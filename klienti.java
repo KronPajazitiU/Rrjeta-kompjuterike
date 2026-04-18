0f4584dbefc2136dfdf32dabad84caa46ac894ed
KronPajazitiU <kron.pajaziti@student.uni-pr.edu>
2026-04-18 21:24:41 +0200

    Update klienti.java

--- klienti.java
+++ klienti.java
@@ -1,1 +1,17 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class klienti {
+    public static void main(String[] args) {
+        // Përcaktimi i saktë i Portit dhe IP-së së Serverit
+        String serverIP = "192.168.1.16";
+        int serverPort = 9997;
+
+        try (
+                // Krijimi i socket
+                Socket socket = new Socket(serverIP, serverPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                Scanner scanner = new Scanner(System.in)
+        )
